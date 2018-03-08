02d4593fbc6c8eac67e5cc0cb782cae145295f25
Sreeja Thota <Sreeja.Thota@cerner.com>
2018-03-07 20:32:33 -0600

    Added list of heros

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROS } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
@@ -6,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  heros = HEROS;
   constructor() { }
 
   ngOnInit() {
-	  hero = "wind";
+	  
   }
 
 }
